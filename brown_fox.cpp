0209fff79bdb665150cac2ed82b2bfec88f97474
SafaS1234 <157080937+SafaS1234@users.noreply.github.com>
2024-02-18 21:08:12 -0600

    Update brown_fox.cpp
    The user can pick if the search is case sensitive or not

--- brown_fox.cpp
+++ brown_fox.cpp
@@ -5,9 +5,35 @@
 using namespace std;
 
 //function for finding the placement of the user input (letter or word)
-int placement_num(const string& str, string& user_input)
+int placement_num(const string& str, const string& user_input, 
+                  bool case_sensitive = true)
 {
-    size_t place = str.find(user_input);
+    size_t place;
+    string str_low, input_low;
+
+    if (case_sensitive)
+    {
+        place = str.find(user_input); 
+    }
+
+    else
+    {
+        str_low = str;
+        input_low = user_input;
+
+        for (char& c : str_low)
+        {
+            c = tolower(c);
+        }
+
+        for (char& c : input_low)
+        {
+            c = tolower(c);
+        }
+
+        place = str_low.find(input_low);
+    }
+
 
     //finds the place where the input occurs
     if (place != string::npos)
@@ -28,9 +54,18 @@ int main()
            user_input;
     int char_place,
         substring_place;
+    char yn;
 
+    cout<< sentence;
     cout << "Enter a letter or word to find: \n";
     getline(cin, user_input);
+
+    cout << "Case-sensitive? Enter y or n";
+    cin >> yn;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    bool case_sensitive = (yn == 'y' || yn == 'Y');
+    
 
     if (user_input.size()==1)
     {
